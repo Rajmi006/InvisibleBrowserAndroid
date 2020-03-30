cb1c7cfb5307cf35f855cc5ce91f947cdd6d3983
Rajmund006 <gallszollosirajmund@gmail.com>
2020-03-30 20:58:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/invisiblebrowser/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.invisiblebrowser;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+Window window;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(Build.VERSION.SDK_INT>=2) {
+            window = this.getWindow();
+            window.setStatusBarColor(this.getResources().getColor(R.color.red));
+        }
+
+
+
+    }
+    public void openTwitter(View view){
+        Intent launchIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https:/www.facebook.com"));
+        startActivity(launchIntent);
+
+    }
+
+}
